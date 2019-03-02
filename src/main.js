8b1c533ec5debdec22d713d300ff504a7aecba5a
houxy <houxy@yonyou.com>
2019-03-02 12:05:25 +0800

    头部公共样式提取，配置webpack.base中的resolve中的路径，引入iconfont字体图标

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'//重置css样式文件
-import './assets/styles/border.css'//1px边框问题
+
+//自己在webpack.base中resolve中修改了配置项，所以下面的引用方式有点改变
+import './assets/styles/reset.css'//重置css样式文件,之前引用的写法
+import 'styles/border.css'//1px边框问题
+import 'styles/iconfont.css'//引入iconfont图标
 import fastClick from 'fastclick'//解决点击300毫秒延时的问题
 
 Vue.config.productionTip = false;
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 <script>
@@ -16,21 +24,29 @@
   }
 </script>
 <style lang="stylus" scoped>
+  //@import '../../../assets/styles/varibles.styl'//可以写成下面的形式
+  //@import '~@/assets/styles/varibles.styl'//@代表src的目录（当引入是js的时候），而css样式时~@为src目录
+  @import '~styles/varibles.styl'//在build中webpack.base中配置别名项，在resolve中设置
+
   .header
     display: flex
     height: 0.86rem
     line-height: 0.86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       width: 0.64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: 0.4rem
     .header-input
       flex: 1
       height: 0.64rem
       line-height: 0.64rem
       margin-top: 0.12rem
       margin-left: 0.2rem
+      padding-left: 0.2rem
       background: #ffffff
       border-radius 0.1rem
       color: #ccc
@@ -38,4 +54,7 @@
       width: 1.24rem
       float: right
       text-align: center
+      .arrow-icon
+        margin-left: -0.04rem
+        font-size: 0.24rem
 </style>
