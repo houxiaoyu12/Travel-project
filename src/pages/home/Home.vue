a3c23db041c29593fcee21b9d93ec9ea51537492
houxy <houxy@yonyou.com>
2019-03-03 17:46:51 +0800

    '周末组件完成'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
       <home-swiper></home-swiper>
       <home-icons></home-icons>
       <home-recommend></home-recommend>
+      <home-weekend></home-weekend>
     </div>
 </template>
 <script>
@@ -11,6 +12,7 @@
     import HomeSwiper from './components/Swiper'
     import HomeIcons from './components/Icons'
     import HomeRecommend from './components/Recommend'
+    import HomeWeekend from './components/Weekend'
 
     export default {
         name: "Home",
@@ -23,7 +25,8 @@
           HomeHeader,
           HomeSwiper,
           HomeIcons,
-          HomeRecommend
+          HomeRecommend,
+          HomeWeekend
         }
     }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">周末推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'HomeWeekend',
+    data() {
+      return {
+        recommendList: [
+          {
+            id: '0001',
+            imgUrl: require('../../../assets/img/1.jpg'),
+            title: '大连圣亚海洋世界',
+            desc: '浪漫大连首站,烂漫的海洋主题乐园'
+
+          },
+          {
+            id: '0002',
+            imgUrl: require('../../../assets/img/2.jpg'),
+            title: '大连圣亚海洋世界',
+            desc: '浪漫大连首站,烂漫的海洋主题乐园'
+
+          },
+          {
+            id: '0003',
+            imgUrl: require('../../../assets/img/3.jpg'),
+            title: '大连圣亚海洋世界',
+            desc: '浪漫大连首站,烂漫的海洋主题乐园'
+
+          },
+          {
+            id: '0004',
+            imgUrl: require('../../../assets/img/1.jpg'),
+            title: '大连圣亚海洋世界',
+            desc: '浪漫大连首站,烂漫的海洋主题乐园'
+
+          },
+        ]
+      }
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: 0.2rem
+    line-height: 0.8rem
+    background: #eee
+    text-indent: 0.2rem //首行缩进
+  .item-img-wrapper
+    overflow: hidden
+    height:0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: 0.1rem
+    .item-title
+      line-height: 0.54rem
+      font-size: 0.32rem
+      ellipsis()
+    .item-desc
+      line-height: 0.4rem
+      color: #ccc
+      ellipsis()
+
+</style>
