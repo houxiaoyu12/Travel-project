90341adb1f7aff3a410b09e20e690fcffe20cd51
houxy <1531604938@qq.com>
2019-03-04 20:58:05 +0800

    '城市搜索逻辑完成'

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,15 +1,77 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名称或拼音">
+    <div>
+        <div class="search">
+            <input v-model="keyword"
+                   class="search-input"
+                   type="text"
+                   placeholder="请输入城市名称或拼音"
+            >
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li
+                    class="search-item "
+                    v-for="item of list"
+                    :key="item.id"
+                >
+                    {{item.name}}
+                </li>
+                <li
+                    class="search-item border-bottom"
+                    v-show="hasNoData"
+                >
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
+    import Bscroll from 'better-scroll'
     export default {
         name: 'CitySearch',
+        props: {
+            cities: Object,
+        },
         data() {
             return {
+                keyword: '',
+                list: [],
+                timer: null,
+            }
+        },
+        computed: {
+            hasNoData() {
+                return !this.list.length
+            }
+        },
+        watch: {
+            keyword () {
+                if(this.timer){//函数节流
+                    clearTimeout(this.timer)
+                }
+                if (!this.keyword){
+                    this.list = [];
+                    return
+                }
+                this.timer = setTimeout(() => {
+                    const result = [];
+                    for (let i in this.cities) {//拿出所有字母
+                        this.cities[i].forEach((value) => {//拿出字母对应的数组
+                            if(value.spell.indexOf(this.keyword) > -1 ||
+                                value.name.indexOf(this.keyword) > -1){
+                                result.push(value)
+                            }
+                        })
+                    }
+                    this.list = result;
+                    //console.log(this.list)
+                },100);
 
             }
+        },
+        mounted () {
+            this.scroll = new Bscroll(this.$refs.search)
         }
     }
 </script>
@@ -28,5 +90,18 @@
             text-align: center
             border-radius: 0.06rem
             color: #666
-
+    .search-content
+        overflow hidden
+        z-index: 1
+        position: absolute
+        top: 1.78rem
+        left: 0
+        right: 0
+        bottom: 0
+        background: #eee
+        .search-item
+            line-height: 0.62rem
+            padding-left: 0.2rem
+            background: #ffffff
+            color: #666
 </style>
