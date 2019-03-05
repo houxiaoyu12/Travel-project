4c091efa321b7690e7dae3e226cf885a5f9336a1
houxy <1531604938@qq.com>
2019-03-05 19:57:38 +0800

    'keep-alive和activated配合使用'用通过判断不等调用后台数据使之重新刷新'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,16 +14,21 @@
     import HomeRecommend from './components/Recommend'
     import HomeWeekend from './components/Weekend'
     import axios from 'axios'
+    import { mapState } from 'vuex'
 
     export default {
         name: "Home",
         data() {
             return {
+              lastCity: '',
               swiperList: [],//轮播图数组
               iconList: [],//轮播选项数组
               recommendList: [],//推荐选项数组
               weekendList: [],//周末选项数组
             }
+        },
+        computed: {
+            ...mapState(['city'])
         },
         components: {//注册组件
           HomeHeader,
@@ -34,7 +39,7 @@
         },
         methods: {
           getHomeInfo () {
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city=' + this.city)
               .then(this.getHomeInfoSucc)
           },
           getHomeInfoSucc (res) {
@@ -50,8 +55,16 @@
           }
         },
         mounted () {
-          this.getHomeInfo()//获取后台数据
+            this.lastCity = this.city;
+            this.getHomeInfo()//获取后台数据
         },
+        activated () {//当页面重新被显示时，activated会被执行
+            if (this.lastCity !== this.city){ //当点击的城市和之前存储的城市不一时
+                this.lastCity = this.city;
+                this.getHomeInfo()
+            }
+            //console.log('activated')
+        }
 
     }
 </script>
