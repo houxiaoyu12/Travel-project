3738bb7eefa490ff9dd53c1c9d153c575eeef1ab
houxy <1531604938@qq.com>
2019-03-06 18:56:36 +0800

    '实现详情页面的渐隐渐现效果'

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content">
+
+        </div>
     </div>
 </template>
 <script>
     import DetailBanner from './components/Banner'
+    import DetailHeader from './components/Header'
+
     export default {
         name: 'Detail',
         data() {
@@ -12,9 +18,11 @@
         },
         components: {
             DetailBanner,
+            DetailHeader
         }
     }
 </script>
-<style lang="stylus" rel="stylesheet/stylus">
-
+<style lang="stylus" scoped>
+    .content
+        height: 50rem
 </style>
