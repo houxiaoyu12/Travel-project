768dcae757bb94159a1220a5d1349aaed8eb4755
houxy <1531604938@qq.com>
2019-03-07 15:44:04 +0800

    '修改Gallary中的图片bug'

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -6,7 +6,7 @@
                     v-for="(item,index) of imgs"
                     :key="index"
                 >
-                    <img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg">
+                    <img class="gallary-img" :src="item">
                 </swiper-slide>
 
                 <div class="swiper-pagination"  slot="pagination"></div>
@@ -61,7 +61,7 @@
         .wrapper
             height: 0
             width: 100%
-            padding-bottom: 55%
+            padding-bottom: 100%
             .gallary-img
                 width: 100%
             .swiper-pagination
